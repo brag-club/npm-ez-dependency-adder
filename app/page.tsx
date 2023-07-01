12244a22e1e6c269a2385ac88e71fca434df823f
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-01 22:09:43 +0530

    updated tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Button from "@/components/ui/Button";
 
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-background">
             <Button>Button</Button>
         </main>
     )
