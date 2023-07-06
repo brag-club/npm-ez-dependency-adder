53220a76bcbb86d8e458d70b87b319c1b812b701
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-06 23:26:25 +0530

    improved contrast in contributors link

--- components/Contributors.tsx
+++ components/Contributors.tsx
@@ -5,19 +5,19 @@ function Contributors() {
         <div className="mt-10 flex w-full justify-center">
             <p className="text-sm text-gray-500">
                 Made with ❤️ by
-                <a href="https://github.com/chirag3003" className="text-blue-500">
+                <a href="https://github.com/chirag3003" className="text-blue-700">
                     {" "}
                     Chirag Bhalotia{" "}
                 </a>
                 and
-                <a href="https://github.com/bravo68web" className="text-blue-500">
+                <a href="https://github.com/bravo68web" className="text-blue-700">
                     {" "}
                     Jyotirmoy Bandopadhayaya
                 </a>
                 . ⭐ Star this repo on
                 <a
                     href="https://github.com/chirag3003/npm-ez-dependency-adder"
-                    className="text-blue-500"
+                    className="text-blue-700"
                 >
                     {" "}
                     Github
