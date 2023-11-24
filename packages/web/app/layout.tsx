4fb3eb325875daf9b8dde4295975e9a67fbdcaf8
Jyotirmoy Bandyopadhyaya [Bravo68] <jbandyopadhayaya@gmail.com>
2023-11-25 03:39:23 +0530

    pack: Added Monorepo arch

--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -1,7 +1,7 @@
+import "./globals.css";
+
 import { Metadata } from "next";
 import { Roboto_Mono } from "next/font/google";
-
-import "./globals.css";
 
 const roboto = Roboto_Mono({ subsets: ["latin"] });
 
--- packages/web/components/DependenciesList.tsx
+++ packages/web/components/DependenciesList.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-
 import { XMarkIcon } from "@heroicons/react/24/outline";
+import React from "react";
 
 import Button from "./ui/Button";
 
