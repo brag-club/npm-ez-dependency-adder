8dc52093839e1edfa0500a75124dd7453f7be6b4
bravo68web <jbandyopadhayaya@gmail.com>
2024-09-18 08:40:18 +0000

    chore: minor code fixes

--- apps/web/components/Contributors.tsx
+++ apps/web/components/Contributors.tsx
@@ -5,16 +5,20 @@ function Contributors() {
         <div className="mt-10 flex w-full justify-center">
             <p className="text-sm text-gray-500">
                 Made with ❤️ by
+                {" "}
                 <a href="https://github.com/chirag3003" className="text-blue-700">
-                    {" "}
-                    Chirag Bhalotia{" "}
+                    Chirag Bhalotia
                 </a>
+                {" "}
                 and
+                {" "}
                 <a href="https://github.com/bravo68web" className="text-blue-700">
                     {" "}
                     Jyotirmoy Bandopadhayaya
                 </a>
+                {" "}
                 . ⭐ Star this repo on
+                {" "}
                 <a
                     href="https://github.com/chirag3003/npm-ez-dependency-adder"
                     className="text-blue-700"
@@ -22,6 +26,7 @@ function Contributors() {
                     {" "}
                     Github
                 </a>
+                {" "}
                 .
             </p>
         </div>
